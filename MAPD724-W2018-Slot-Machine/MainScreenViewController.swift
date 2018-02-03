d538db0450e7940b0807e1167504e0ea5bd4cb68
Tejal1386 <tejalmca1386@gmail.com>
2018-02-03 01:43:13 -0500

    Define variables, outlets and picker view settings

--- MAPD724-W2018-Slot-Machine/MainScreenViewController.swift
+++ MAPD724-W2018-Slot-Machine/MainScreenViewController.swift
@@ -8,82 +8,171 @@
 
 import UIKit
 
-class MainScreenViewController: UIViewController, UIPickerViewDelegate,UIPickerViewDataSource {
-   
 
-    @IBOutlet weak var fruitTraypickerView: UIPickerView!
-    @IBOutlet weak var spinButton: UIButton!
+
+class MainScreenViewController: UIViewController, UIPickerViewDelegate,UIPickerViewDataSource {
+  
+  
+    // ---------------------------------------------- ---------------------------------------------- ----------------------------------------------
+    //                                                               Variables And Outlets
+    // ---------------------------------------------- ---------------------------------------------- ----------------------------------------------
     
-    func reandomNumber(num: Int) -> Int {
-        return Int(arc4random_uniform(UInt32(num)))
+    var player_Money: Int = 1000
+    var winnings:Int = 0
+    var jackpot:Int = 5000
+    var turn:Int = 0
+    var playerBet:Int = 1
+    var winNumber:Int = 0
+    var lossNumber:Int = 0
+    var WinRatio:Int = 0
+    
+    var element1: Int = 0
+    var element2: Int = 0
+    var element3: Int = 0
+    var element4: Int = 0
+    var element5: Int = 0
+    var element:[Int] = [0]
+    
+    
+    @IBOutlet weak var playerLoose: UILabel!
+    @IBOutlet weak var playerWins: UILabel!
+    @IBOutlet weak var player_bet: UILabel!
+    @IBOutlet weak var ReelPickerView: UIPickerView!
+    @IBOutlet weak var spinButton: UIButton!
+    @IBOutlet weak var winNumberLabel: UILabel!
+    @IBOutlet weak var playerMoney: UILabel!
+    
+    //Function generate  random number
+    func reandomNumber() -> Int {
+        let lower : UInt32 = 20
+        let upper : UInt32 = 150
+        let randomNumber = arc4random_uniform(upper - lower) + lower
+        return Int(randomNumber)
     }
+    
+   // ---------------------------------------------- ---------------------------------------------- ----------------------------------------------
+   //                                                               ViewDidLoad
+   // ---------------------------------------------- ---------------------------------------------- ----------------------------------------------
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    @IBAction func spinButtonClick(_ sender: UIButton) {
-        fruitTraypickerView.selectRow((reandomNumber(num: 8)) , inComponent: 0, animated: true)
-        fruitTraypickerView.selectRow((reandomNumber(num: 8)) , inComponent: 1, animated: true)
-        fruitTraypickerView.selectRow((reandomNumber(num: 8)) , inComponent: 2, animated: true)
         
     }
     
+    
+    // ---------------------------------------------- ---------------------------------------------- ----------------------------------------------
+    //                                                               PickerView Settings (Reel)
+    // ---------------------------------------------- ---------------------------------------------- ----------------------------------------------
     
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 3
+        return 5
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 200
     }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 8
-    }
+    //Defining width and height of component in pickerview
     func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
-        return 130.0
+        return 60.0
     }
     func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
-        return 130.0
+        return 60.0
     }
     
+    
+    // setting number of elements as an image in Reel
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        let myView = UIView(frame: CGRect(x:0,y:0, width:130, height:130))
+        let myView = UIView(frame: CGRect(x:0,y:0, width:60, height:60))
         
-        let myImageView = UIImageView(frame: CGRect(x:0,y:0,width:130,height:130))
+        let myImageView = UIImageView(frame: CGRect(x:0,y:0,width:60,height:60))
         
-        switch row {
-        case 0:
-            myImageView.image = UIImage(named:"apple.png")
-        case 1:
-            myImageView.image = UIImage(named:"banana.png")
-        case 2:
-            myImageView.image = UIImage(named:"grapes.png")
-        case 3:
-            myImageView.image = UIImage(named:"cherry.png")
-        case 4:
-            myImageView.image = UIImage(named:"strawberry.png")
-        case 5:
-            myImageView.image = UIImage(named:"peach.png")
-        case 6:
-            myImageView.image = UIImage(named:"bell.png")
-        case 7:
-            myImageView.image = UIImage(named:"Seven.png")
-        default:
-            myImageView.image = UIImage(named:"Seven.png")
+        
+        var Banana:Int=0
+        var Grape:Int=1
+        var Strawberry:Int=2
+        var Peaches:Int=3
+        var Bell:Int=4
+        var Seven:Int=5
+        var Apple:Int=6
+        
+        for _ in 6 ... 200 {
+            Apple=Apple+7
+            
+            switch row {
+                
+            case Banana:
+               
+                myImageView.image = UIImage(named:"banana.png")
+            
+            case Grape:
+               
+                myImageView.image = UIImage(named:"grapes.png")
+                
+            case Strawberry:
+                
+                myImageView.image = UIImage(named:"strawberry.png")
+                
+            case Peaches:
+                
+                myImageView.image = UIImage(named:"peach.png")
+                
+            case Bell:
+             
+                myImageView.image = UIImage(named:"bell.png")
+                
+            case Seven:
+               
+                myImageView.image = UIImage(named:"Seven.jpg")
+                
+            default :
+               
+                myImageView.image = UIImage(named:"apple.png")
+                Banana=Banana+7
+                Grape=Grape+7
+                Strawberry=Strawberry+7
+                Peaches=Peaches+7
+                Bell=Bell+7
+                Seven=Seven+7
+                
+            }
         }
-        
         myView.addSubview(myImageView)
         
         return myView
     }
     
+    // ---------------------------------------------- ---------------------------------------------- ----------------------------------------------
+    //                                                               Action Function
+    // ---------------------------------------------- ---------------------------------------------- ----------------------------------------------
     
     
-
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        
+    
+    }
+    
+    @IBAction func spinButtonPreesed(_ sender: UIButton) {
+        ReelPickerView.selectRow((reandomNumber()) , inComponent: 0, animated: true)
+        ReelPickerView.selectRow((reandomNumber()) , inComponent: 1, animated: true)
+        ReelPickerView.selectRow((reandomNumber()) , inComponent: 2, animated: true)
+        ReelPickerView.selectRow((reandomNumber()) , inComponent: 3, animated: true)
+        ReelPickerView.selectRow((reandomNumber()) , inComponent: 4, animated: true)
+        
+        
+        playerBet = Int(player_bet.text!)!
+        
+      if (playerBet <= player_Money) {
+            
+            // play
+        }
+        else {
+            //  pay more money to continue spins
+        }
+        
+    }
+    
+   
 }
